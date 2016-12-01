ac47e39b19f8820de1739919584f83a04ed3310f
Viet <vietsemantics@gmail.com>
2016-12-01 04:41:55 -0600

    add code

--- action.js
+++ action.js
@@ -6,13 +6,34 @@
 
 jQuery(document).ready(function ($) {
 
-    var x = document.querySelector("button .navbar-toggle");
-    if (x == null)
-        window.alert(" Over me");
 
-    $("button > .navbar-toggle").hover(function () {
-        window.alert(" Over me");
-        $(".navbar-default, .navbar-default .navbar-collapse").css('display','block');
+    $("#btn-menu").hover(function () {
+        // window.alert(" Over me");
+        // $(".navbar-default .navbar-collapse").css('display','block');
+    }, function () {
+        // $(".navbar-default .navbar-collapse").css('display','none');
     });
 
+
+        // var clickover = $(event.target);
+
+
+        //
+
+        //
+        // if (!clickover.hasClass(".navbar-default .navbar-collapse")) {
+        //     // $("button.navbar-toggle").click();
+        //     var _opened = ( $(".navbar-collapse").css('display') == 'block');
+        //     if (_opened) {
+        //         window.alert("ABCD");
+        //         $(".navbar-default .navbar-collapse").css('display', 'none');
+        //     }
+        // }
+
+});
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
 });
